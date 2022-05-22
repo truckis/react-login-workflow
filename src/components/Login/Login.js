69f7836a271a4eebc36a8de5d3e6ff4b59549c54
Truckis <truckis@Truckiss-MacBook-Pro.local>
2022-05-22 12:28:20 -0700

    login-in workflow with validation, pre useReducer

---
+++ src/components/Login/Login.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect, useReducer } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const emailReducer = (state, action) => {
+  return {value: '', isValid: false};
+};
+
+const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: false});
+
+
+  useEffect(() => {
+    console.log('EFFECT RUNNING')
+  }, [enteredPassword])
+
+  // Will only rerender component if any of the dependencies change (kinda if like addEventListener). You can omit useState functions from dependencies
+  useEffect(() => {
+   const identifier = setTimeout(() => {
+     console.log('Checking form Validity!');
+     setFormIsValid(
+      enteredPassword.trim().length > 6 && enteredEmail.includes('@')
+    );
+   }, 500);
+
+   return () => {
+     console.log('CLEANUP')
+     clearTimeout(identifier)
+   }
+  }, [enteredPassword, enteredEmail ])
+
+  /* Adding the settime out above allows to check the validity every 500ms instead of every keystroke. This greatly improves performance */
+
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const validateEmailHandler = () => {
+    setEmailIsValid(enteredEmail.includes('@'));
+  };
+
+  const validatePasswordHandler = () => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredEmail, enteredPassword);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
